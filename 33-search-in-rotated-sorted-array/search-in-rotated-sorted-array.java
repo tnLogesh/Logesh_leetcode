f0a6122215aacb538c844a50674ef8619feb33ba
Logesh <147993312+tnLogesh@users.noreply.github.com>
2025-04-16 15:21:22 +0530

    Time: 0 ms (100.00%) | Memory: 42 MB (68.24%) - LeetSync

---
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        int n=nums.length;
+        for(int i=0;i<n;i++){
+            if(nums[i]==target){
+                return i;
+            }
+            if(nums[n-i-1]==target){
+                return n-i-1;
+            }
+        }
+        return -1;
+    }
+}
