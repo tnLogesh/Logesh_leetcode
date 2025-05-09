a44632b2cf6b23acad251308cbf7757cb0620c03
Logesh <147993312+tnLogesh@users.noreply.github.com>
2025-05-09 07:13:19 +0530

    Time: 2 ms (82.59%) | Memory: 41.5 MB (71.89%) - LeetSync

--- 2442-number-of-arithmetic-triplets/number-of-arithmetic-triplets.java
+++ 2442-number-of-arithmetic-triplets/number-of-arithmetic-triplets.java
@@ -4,9 +4,9 @@ class Solution {
         int n=nums.length;
         for(int i=0;i<n-1;i++){
             for(int j=i+1;j<n-1;j++){
-                if(Math.abs(nums[i]-nums[j])==diff){
+                if(nums[j]-nums[i]==diff){
                 for(int k=j+1;k<n;k++){
-                    if(Math.abs(nums[j]-nums[k])==diff)count++;
+                    if(nums[k]-nums[j]==diff)count++;
                 }
                 }
             }
