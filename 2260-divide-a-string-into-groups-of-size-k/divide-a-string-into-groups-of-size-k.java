44e02c34a07ee9e4a98be70768f4f4eb504166e0
Logesh <147993312+tnLogesh@users.noreply.github.com>
2025-06-22 16:39:30 +0530

    Time: 8 ms (10.54%) | Memory: 43 MB (26.20%) - LeetSync

---
+++ 2260-divide-a-string-into-groups-of-size-k/divide-a-string-into-groups-of-size-k.java
@@ -0,0 +1,16 @@
+class Solution {
+    public String[] divideString(String s, int k, char fill) {
+        int n=s.length();
+        while(n%k!=0){
+            s+=fill;
+            n=s.length();
+        }
+        System.out.println(s);
+        String[] arr=new String[n/k];
+        int a=0;
+        for(int i=0;i<n;i+=k){
+            arr[a++]=s.substring(i,i+k);
+        }
+        return arr;
+    }
+}
