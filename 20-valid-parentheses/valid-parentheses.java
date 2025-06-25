0bab6a355b0cfdb7165d4e2d4851cdf2e58138d8
Logesh <147993312+tnLogesh@users.noreply.github.com>
2025-06-25 14:46:13 +0530

    Time: 2 ms (96.80%) | Memory: 41.6 MB (81.67%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.java
+++ 20-valid-parentheses/valid-parentheses.java
@@ -1,18 +1,28 @@
 class Solution {
     public boolean isValid(String s) {
-        int len=s.length();
-        char[]arr=s.toCharArray();
-        Stack<Character> st=new Stack<>();
-        for(int i=0;i<len;i++){
-            if(arr[i]=='{'||arr[i]=='('||arr[i]=='['){
-                st.push(arr[i]);
+        Stack<Character>st=new Stack<>();
+        int n=s.length();
+        char ch=s.charAt(0);
+        if(ch==')'||ch=='}'||ch==']'||n%2!=0){
+            return false;
+        }
+        for(int i=0;i<n;i++){
+            ch=s.charAt(i);
+            if(ch=='('||ch=='{'||ch=='['){
+                st.push(ch);
             }
             else{
-                if(st.isEmpty()||
-                (arr[i]=='}'&&st.pop()!='{')||
-                (arr[i]==']'&&st.pop()!='[')||
-                (arr[i]==')'&&st.pop()!='(')){
-                    return false;}
+               if(!st.isEmpty()){
+                    switch(ch){
+                    case ')':{if(st.peek()=='('){st.pop();break;}else return false;}
+                    case '}':{if(st.peek()=='{'){st.pop();break;}else return false;}
+                    case ']':{if(st.peek()=='['){st.pop();break;}else return false;}
+                    default:{return false;}
+                }
+               }
+               else {
+                return false;
+               }
             }
         }
         return st.isEmpty();
