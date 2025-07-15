0d84963977a3fc16d6e7fe1121181e299611892c
Logesh <147993312+tnLogesh@users.noreply.github.com>
2025-07-15 09:35:49 +0530

    Time: 6 ms (11.04%) | Memory: 43.7 MB (5.96%) - LeetSync

--- 3396-valid-word/valid-word.java
+++ 3396-valid-word/valid-word.java
@@ -5,14 +5,12 @@ class Solution {
         }
         int v=0;
         int c=0;
-        int n=0;
         word=word.replaceAll("[aeiouAEIOU]","a");
-        word=word.replaceAll("[1234567890]","1");
         word=word.replaceAll("[bcdfghjklmnpqrstvwxyzBCDFGHJKLMNPQRSTVWXYZ]","b");
         for(char i:word.toCharArray()){
-            if(i=='1')n++;
-            else if(i=='a')v++;
+            if(i=='a')v++;
             else if(i=='b')c++;
+            else if(i>='0'&&i<='9')continue;
             else{
                 return false;
             }
