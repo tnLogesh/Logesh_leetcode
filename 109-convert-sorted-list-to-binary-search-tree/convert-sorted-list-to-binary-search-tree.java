dec27d4f33a858c6a4e2460e1d8ee93f095b9a26
Logesh <147993312+tnLogesh@users.noreply.github.com>
2025-07-23 14:41:01 +0530

    Time: 1 ms (27.04%) | Memory: 45.1 MB (37.14%) - LeetSync

---
+++ 109-convert-sorted-list-to-binary-search-tree/convert-sorted-list-to-binary-search-tree.java
@@ -0,0 +1,46 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode buildBalBST(List<Integer>node,int start,int end){
+        if(start>end)return null;
+        int mid=start+(end-start)/2;
+        TreeNode root=new TreeNode(node.get(mid));
+        root.left=buildBalBST(node,start,mid-1);
+        root.right=buildBalBST(node,mid+1,end);
+        return root;
+    }
+    private void inOrder(ListNode head,List<Integer>l){
+        while(head!=null){
+            l.add(head.val);
+            head=head.next;
+        }
+    }
+    public TreeNode sortedListToBST(ListNode head) {
+        List<Integer>node=new ArrayList<>();
+        inOrder(head,node);
+        return buildBalBST(node,0,node.size()-1);
+    }
+}
