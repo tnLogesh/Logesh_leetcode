4bfd7be98507c1c6cc6810bf6c878646cc9c7a8b
Logesh <147993312+tnLogesh@users.noreply.github.com>
2025-07-25 19:04:18 +0530

    Time: 1119 ms (6.80%) | Memory: 0B (100.00%) - LeetSync

--- 1155-product-sales-analysis-iii/product-sales-analysis-iii.sql
+++ 1155-product-sales-analysis-iii/product-sales-analysis-iii.sql
@@ -1,5 +1,5 @@
 # Write your MySQL query statement below
-select distinct product_id,year as first_year,quantity,price
+select product_id,year as first_year,quantity,price
 from sales
 where (product_id,year) in (select product_id,min(year)
 from sales
