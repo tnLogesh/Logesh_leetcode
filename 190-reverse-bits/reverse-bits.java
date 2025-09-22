68d9eb7ea3d9e8b7fcc8d31d8a0956870ee50fa6
Logesh <147993312+tnLogesh@users.noreply.github.com>
2025-09-22 19:08:23 +0530

    Time: 0 ms (100.00%) | Memory: 40.8 MB (63.37%) - LeetSync

---
+++ 190-reverse-bits/reverse-bits.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int reverseBits(int n) {
+        int res=0;
+        for(int i=0;i<32;i++){
+            int bit=n&1;
+            res=(res<<1)|bit;
+            n>>>=1;
+        }
+        return res;
+    }
+}
