aab0d16527a07eea200fe3e8cc9d858b744434ed
Logesh <147993312+tnLogesh@users.noreply.github.com>
2025-11-13 10:45:52 +0530

    Time: 7 ms (78.52%) | Memory: 47.9 MB (5.19%) - LeetSync

---
+++ 3493-maximum-number-of-operations-to-move-ones-to-the-end/maximum-number-of-operations-to-move-ones-to-the-end.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int maxOperations(String s) {
+        int ans = 0;
+        int ones = 0;
+        int n = s.length();
+        for (int i = 0; i < n; i++) {
+            if (s.charAt(i) == '1') {
+                ones++;
+            } else {
+                if (i == n - 1 || s.charAt(i + 1) == '1') {
+                    ans += ones;
+                }
+            }
+        }
+        return ans;
+    }
+}
