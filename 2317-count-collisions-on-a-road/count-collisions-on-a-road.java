bc48b663188cb2a085e9bb17b3cb0d69adcb6503
Logesh <147993312+tnLogesh@users.noreply.github.com>
2025-12-04 14:06:00 +0530

    Time: 10 ms (93.15%) | Memory: 47.7 MB (18.15%) - LeetSync

--- 2317-count-collisions-on-a-road/count-collisions-on-a-road.java
+++ 2317-count-collisions-on-a-road/count-collisions-on-a-road.java
@@ -1,37 +1,11 @@
 class Solution {
     public int countCollisions(String directions) {
-        Stack<Character> st = new Stack<>();
-        int collision = 0;
-        for (char ch : directions.toCharArray()) {
-            if (ch == 'L') {
-                if (!st.isEmpty() && st.peek() == 'R') {
-                    int cntR = 0;
-                    while (!st.isEmpty() && st.peek() == 'R') {
-                        st.pop();
-                        cntR++;
-                    }
-                    collision += cntR + 1;
-                    st.push('S');
-                } else if (!st.isEmpty() && st.peek() == 'S') {
-                    collision++;
-                } else {
-                    st.push('L');
-                }
-            } else if (ch == 'S') {
-                if (!st.isEmpty() && st.peek() == 'R') {
-                    int cntR = 0;
-                    while (!st.isEmpty() && st.peek() == 'R') {
-                        st.pop();
-                        cntR++;
-                    }
-                    collision += cntR;
-                    st.push('S');
-                } else {
-                    if (st.isEmpty() || st.peek() != 'S') st.push('S');
-                }
-            } else {
-                st.push('R');
-            }
+        int n=directions.length(),i=0,j=n-1;
+        while(i<n&&directions.charAt(i)=='L')i++;
+        while(j>=0&&directions.charAt(j)=='R')j--;
+        int collision=0;
+        for(int a=i;a<=j;a++){
+            if(directions.charAt(a)!='S')collision++;
         }
         return collision;
     }
