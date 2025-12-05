6c696952c86c95cb7d163340de714f74790246ba
Logesh <147993312+tnLogesh@users.noreply.github.com>
2025-12-05 14:09:57 +0530

    Time: 1 ms (98.85%) | Memory: 43.5 MB (21.96%) - LeetSync

--- 3636-check-balanced-string/check-balanced-string.java
+++ 3636-check-balanced-string/check-balanced-string.java
@@ -3,10 +3,11 @@ class Solution {
         int oddSum=0,evenSum=0;
         int n=num.length();
         for(int i=0;i<n;i++){
+            int digit=num.charAt(i)-'0';
             if(i%2==0){
-                evenSum+=num.charAt(i)-'0';
+                evenSum+=digit;
             }
-            else oddSum+=num.charAt(i)-'0';
+            else oddSum+=digit;
         }
         return oddSum==evenSum;
     }
