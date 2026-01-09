b2a06fb214ff45cc9c144b45f1665840cbda15e1
Logesh <147993312+tnLogesh@users.noreply.github.com>
2026-01-09 13:58:42 +0530

    Time: 1 ms (100.00%) | Memory: 45.8 MB (73.90%) - LeetSync

--- 3582-find-indices-of-stable-mountains/find-indices-of-stable-mountains.java
+++ 3582-find-indices-of-stable-mountains/find-indices-of-stable-mountains.java
@@ -1,5 +1,11 @@
 class Solution {
-    public List<Integer> stableMountains(int[] height, int threshold) {
+    static{
+        int[]arr=new int[]{1,2,3};
+        for(int i=0;i<100;i++){
+            stableMountains(arr,3);
+        }
+    }
+    public static List<Integer> stableMountains(int[] height, int threshold) {
         List<Integer>l=new ArrayList<>();
         for(int i=1;i<height.length;i++){
             if(height[i-1]>threshold)l.add(i);
