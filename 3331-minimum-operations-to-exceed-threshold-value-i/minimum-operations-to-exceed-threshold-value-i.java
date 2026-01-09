682884e16245038d80d804d0cbd407841b9d0499
Logesh <147993312+tnLogesh@users.noreply.github.com>
2026-01-09 14:04:03 +0530

    Time: 0 ms (100.00%) | Memory: 44.9 MB (15.92%) - LeetSync

--- 3331-minimum-operations-to-exceed-threshold-value-i/minimum-operations-to-exceed-threshold-value-i.java
+++ 3331-minimum-operations-to-exceed-threshold-value-i/minimum-operations-to-exceed-threshold-value-i.java
@@ -1,10 +1,9 @@
 class Solution {
     public int minOperations(int[] nums, int k) {
-        Arrays.sort(nums);
         int result=0,n=nums.length;
         for(int i=0;i<n;i++){
-            if(nums[i]>=k)return i;
+            if(nums[i]<k)result++;
         }
-        return 0;
+        return result;
     }
 }
