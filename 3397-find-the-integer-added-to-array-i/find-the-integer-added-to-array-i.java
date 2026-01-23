eaa46662b4040bf25b5ff8635a7edc9ed97fb7a1
Logesh <147993312+tnLogesh@users.noreply.github.com>
2026-01-23 11:35:44 +0530

    Time: 0 ms (100.00%) | Memory: 45 MB (59.31%) - LeetSync

---
+++ 3397-find-the-integer-added-to-array-i/find-the-integer-added-to-array-i.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int addedInteger(int[] nums1, int[] nums2) {
+        int min1=Integer.MAX_VALUE;
+        int min2=min1;
+        for(int i:nums1){
+            if(i<min1)min1=i;
+        }
+        for(int i:nums2){
+            if(i<min2)min2=i;
+        }
+        return min2-min1;
+    }
+}
