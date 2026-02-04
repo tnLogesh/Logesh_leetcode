85e401b3a4ea3ffacf9fea649f3a66d889d02877
Logesh <147993312+tnLogesh@users.noreply.github.com>
2026-02-04 10:00:01 +0530

    Time: 0 ms (100.00%) | Memory: 42.2 MB (65.68%) - LeetSync

--- 1585-the-kth-factor-of-n/the-kth-factor-of-n.java
+++ 1585-the-kth-factor-of-n/the-kth-factor-of-n.java
@@ -1,9 +1,13 @@
 class Solution {
     public int kthFactor(int n, int k) {
-        int index=0;
+        if(k>n)return -1;
+        if(k==1)return 1;
+        int count=0;
         for(int i=1;i<=n;i++){
-            if(n%i==0){index++;}
-            if(index==k)return i;
+            if(n%i==0){
+                count++;
+            }
+            if(count==k)return i;
         }
         return -1;
     }
