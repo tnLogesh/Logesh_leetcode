d09959ca8adbacc3bbbb947ff14566728129b96c
Logesh <147993312+tnLogesh@users.noreply.github.com>
2026-03-25 14:18:22 +0530

    Time: 7 ms (6.47%) | Memory: 116.1 MB (74.98%) - LeetSync

--- 2047-find-a-peak-element-ii/find-a-peak-element-ii.java
+++ 2047-find-a-peak-element-ii/find-a-peak-element-ii.java
@@ -1,77 +1,22 @@
 class Solution {
     public int[] findPeakGrid(int[][] mat) {
-        int[]arr=new int[2];
-        int n=mat.length,m=mat[0].length;
-        if(n==1&&m==1)return arr;
-        else if(n==1){
-            int max=0;
-            for(int i=0;i<m;i++){
-                if(max<mat[0][i]){
-                    max=mat[0][i];
-                    arr[1]=i;
-                }
-            }
-            return arr;
-        }
-        else if(m==1){
-            int max=0;
-            for(int i=0;i<n;i++){
-                if(max<mat[i][0]){
-                    max=mat[i][0];
-                    arr[0]=i;
-                }
-            }
-            return arr;
-        }
+        int n=mat.length;
+        int m=mat[0].length;
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                if(i==0&&j==0){
-                    if(mat[i][j]>mat[i+1][j]&&mat[i][j]>mat[i][j+1]){
+                int up=i==0?-1:mat[i-1][j];
+                int down=i==n-1?-1:mat[i+1][j];
+                int left=j==0?-1:mat[i][j-1];
+                int right=j==m-1?-1:mat[i][j+1];
+                if(mat[i][j]>up&&
+                    mat[i][j]>down&&
+                    mat[i][j]>left&&
+                    mat[i][j]>right)
+                    {
                         return new int[]{i,j};
-                    }
-                }
-                else if(i==n-1&&j==m-1){
-                    if(mat[i][j]>mat[i-1][j]&&mat[i][j]>mat[i][j-1]){
-                        return new int[]{i,j};
-                    }
-                }
-                else if(i==0&&j==m-1){
-                    if(mat[i][j]>mat[i+1][j]&&mat[i][j]>mat[i][j-1]){
-                        return new int[]{i,j};
-                    }
-                }
-                else if(i==n-1&&j==0){
-                    if(mat[i][j]>mat[i-1][j]&&mat[i][j]>mat[i][j+1]){
-                        return new int[]{i,j};
-                    }
-                }
-                else if(i==0){
-                    if(mat[i][j]>mat[i][j+1]&&mat[i][j]>mat[i][j-1]&&mat[i][j]>mat[i+1][j]){
-                        return new int[]{i,j};
-                    }
-                }
-                else if(i==n-1){
-                    if(mat[i][j]>mat[i][j-1] && mat[i][j]>mat[i][j+1] && mat[i][j]>mat[i-1][j]){
-                        return new int[]{i,j};
-                    }
-                }
-                else if(j==0){
-                    if(mat[i][j]>mat[i-1][j]&&mat[i][j]>mat[i+1][j]&&mat[i][j]>mat[i][j+1]){
-                        return new int[]{i,j};
-                    }
-                }
-                else if(j==m-1){
-                    if(mat[i][j]>mat[i-1][j] && mat[i][j]>mat[i+1][j] && mat[i][j]>mat[i][j-1]){
-                        return new int[]{i,j};
-                    }
-                }
-                else{
-                    if(mat[i][j]>mat[i-1][j]&&mat[i][j]>mat[i+1][j]&&mat[i][j]>mat[i][j-1]&&mat[i][j]>mat[i][j+1]){
-                        return new int[]{i,j};
-                    }
                 }
             }
         }
-        return arr;
+        return new int[]{-1,-1};
     }
 }
