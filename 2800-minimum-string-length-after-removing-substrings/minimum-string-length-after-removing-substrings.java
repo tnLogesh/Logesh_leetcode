bc02f6a9206819f7076b9610f1aeac6802b8a5ad
Logesh <147993312+tnLogesh@users.noreply.github.com>
2026-04-04 15:54:46 +0530

    Time: 5 ms (63.31%) | Memory: 45.2 MB (16.99%) - LeetSync

---
+++ 2800-minimum-string-length-after-removing-substrings/minimum-string-length-after-removing-substrings.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int minLength(String s) {
+        Stack<Character>stack=new Stack<>();
+        int n=s.length();
+        stack.push(s.charAt(0));
+        for(int i=1;i<n;i++){
+            char cur=s.charAt(i);
+            if(!stack.isEmpty()){
+                char top=stack.peek();
+                if(top=='A'&&cur=='B')stack.pop();
+                else if(top=='C'&&cur=='D')stack.pop();
+                else stack.push(cur);
+            }
+            else stack.push(cur);
+        }
+        // while(!stack.isEmpty()){
+        //     System.out.print(stack.pop());
+        // }
+        return stack.size();
+    }
+}
