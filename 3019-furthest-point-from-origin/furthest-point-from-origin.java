18e83e7725285b28ea0ab48b4af55089db8f16a2
Logesh <147993312+tnLogesh@users.noreply.github.com>
2026-04-24 10:33:48 +0530

    Time: 1 ms (100.00%) | Memory: 44.1 MB (10.00%) - LeetSync

---
+++ 3019-furthest-point-from-origin/furthest-point-from-origin.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int furthestDistanceFromOrigin(String moves) {
+        int L=0;
+        int R=0;
+        int UnderScore=0;
+        for(char ch:moves.toCharArray()){
+            if(ch=='L')L++;
+            else if(ch=='R')R++;
+            else UnderScore++;
+        }
+        return Math.abs(L-R)+UnderScore;
+    }
+}
